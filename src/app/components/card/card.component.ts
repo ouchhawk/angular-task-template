0cb4b836b7c42fc63533984758c4e2ac270d7d3b
ouchhawk <58258129+ouchhawk@users.noreply.github.com>
2023-04-05 19:59:16 +0300

    Make Minor Changes

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,14 +3,12 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Post } from 'src/app/models/post';
 import { PostDetailsComponent } from '../post-details/post-details.component';
 
-
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-
   @Input() post: Post;
 
   constructor(private modalService: NgbModal) { }
@@ -18,9 +16,5 @@ export class CardComponent {
   onClickShowModal(post: Post) {
     const modalRef = this.modalService.open(PostDetailsComponent, { scrollable: true });
     modalRef.componentInstance.post = post;
-  }
-
-  changeCurrentPostClass(post: Post) {
-    return post === this.post ? " inactive" : "";
   }
 }
